2d426a734d2e8ac2e4d715225144f21e3a80b150
wenty2015 <Wenty@Wenty.local>
2015-12-14 01:29:11 -0500

    day2_1

--- day1_2.py
+++ day1_2.py
@@ -5,14 +5,14 @@ i = 0
 stairs = 0
 length = len(str)
 
-while stairs >= 0 and i <= length :
+while stairs >= 0 and i < length :
     if str[i] == '(':
         stairs += 1
     else:
         stairs -= 1
     i += 1
 
-if i > length :
+if i = length :
     print('never enter the basement')
 else:
     print(i)
---
+++ day2_1.py
@@ -0,0 +1,23 @@
+with open('day2.txt','r') as file:
+    array = []
+    for line in file:
+        array.append(line)
+
+area = 0
+i = 0
+
+while i < len(array):
+    comb = []
+
+    i1 = array[i].find('x')
+    comb.append( int(array[i][0:i1]))
+    i2 = array[i].find('x',i1+1)
+    comb.append( int(array[i][i1+1:i2]))
+    comb.append( int(array[i][i2+1:]))
+
+    comb.sort()
+    
+    area += 3* comb[0] * comb[1] + 2 * comb[0] * comb[2] + 2 * comb[1] * comb[2]
+    i += 1
+
+print(area)
